025cab287e56452278e7b2c0801483cc2135580e
KyleUltimate <shiue.kyle@gmail.com>
2022-03-05 20:15:49 +0800

    Fix A bunch of stuff and make things actually work

--- main.cpp
+++ main.cpp
@@ -36,9 +36,10 @@ void exec_print(const char* cmd, const string& error_message="Error!") {
 
 void convert_into_another(){
     cout << "Select a file to convert.(Including file extensions)\n";
-    exec_print("ls");
     string file_name;
-    cin >> file_name;
+    cin.ignore();
+    std::getline(cin,file_name);
+    file_name='\"'+file_name+'\"';
     cout.flush();
     cout << "Select a format to convert to(filename extensions).\n";
     string format;
@@ -49,9 +50,10 @@ void convert_into_another(){
 
 void extract_audio(){
     cout << "Select a file to extract audio from.(Including file extensions)\n";
-    exec_print("ls");
     string file_name;
-    cin >> file_name;
+    cin.ignore();
+    std::getline(cin,file_name);
+    file_name='\"'+file_name+'\"';
     cout << "Do you want to re-encode the audio?(y/n)\n";
     string re_encode;
     cin >> re_encode;
@@ -59,20 +61,21 @@ void extract_audio(){
         cout << "Select a format to convert to(filename extensions).\n";
         string format;
         cin >> format;
-        string command = "ffmpeg -i -map 0:a " + file_name + " " + file_name.substr(file_name.find_last_of(".")+ 1) + "_converted." + format;
+        string command = "ffmpeg -i " + file_name + " -map 0:a " + file_name.substr(file_name.find_last_of(".")+ 1) + "_converted." + format;
         exec_print(command.c_str());
     }
     else{
-        string command = "ffmpeg -i -map 0:a -c copy " + file_name.substr(0, file_name.find_last_of(".")) + "_audio.wav";
+        string command = "ffmpeg -i " + file_name + " -map 0:a -c copy " + file_name.substr(0, file_name.find_last_of(".")) + "_audio.wav";
         exec_print(command.c_str());
     }
 }
 
 void extract_video(){
     cout << "Select a file to extract video from.(Including file extensions)\n";
-    exec_print("ls");
     string file_name;
-    cin >> file_name;
+    cin.ignore();
+    std::getline(cin,file_name);
+    file_name='\"'+file_name+'\"';
     cout << "Do you want to re-encode the video?(y/n)\n";
     string re_encode;
     cin >> re_encode;
@@ -80,37 +83,53 @@ void extract_video(){
         cout << "Select a format to convert to(filename extensions).\n";
         string format;
         cin >> format;
-        string command = "ffmpeg -i -map 0:v " + file_name + " " + file_name.substr(file_name.find_last_of(".")+ 1) + "_extracted." + format;
+        string command = "ffmpeg -i " + file_name + " -map 0:v " + file_name.substr(file_name.find_last_of(".")+ 1) + "_extracted." + format;
         exec_print(command.c_str());
     }
     else{
-        string command = "ffmpeg -i -map 0:v -c copy " + file_name.substr(0, file_name.find_last_of(".")) + "_extracted." + file_name.substr(file_name.find_last_of(".")+ 1);
+        string command = "ffmpeg -i " + file_name + " -map 0:v -c copy " + file_name.substr(0, file_name.find_last_of(".")) + "_extracted." + file_name.substr(file_name.find_last_of(".")+ 1);
         exec_print(command.c_str());
     }
 }
 
-void cut_video(){
+void cut_video_l(){
     cout << "Select a file to cut video from.(Including file extensions)\n";
-    exec_print("ls");
     string file_name;
-    cin >> file_name;
+    cin.ignore();
+    std::getline(cin,file_name);
+    file_name='\"'+file_name+'\"';
     cout << "Select a start time(00:00:02).\n";
     string start_time;
     cin >> start_time;
     cout << "Select an end time(00:00:25) NOT duration.\n";
     string end_time;
     cin >> end_time;
-    string command = "ffmpeg -ss " + start_time + " -i " + file_name + " -to " + end_time + " -c copy " + file_name.substr(0, file_name.find_last_of(".")) + "_cut." + file_name.substr(file_name.find_last_of(".")+ 1);
+    string command = "ffmpeg -ss " + start_time + " -i " + file_name + " -copyts -to " + end_time + " -c copy " + file_name.substr(0, file_name.find_last_of(".")) + "_cut." + file_name.substr(file_name.find_last_of(".")+ 1);
     exec_print(command.c_str());
 }
-
+void cut_video_d(){
+    cout << "Select a file to cut video from.(Including file extensions)\n";
+    string file_name;
+    cin.ignore();
+    std::getline(cin,file_name);
+    file_name='\"'+file_name+'\"';
+    cout << "Select a start time(00:00:02).\n";
+    string start_time;
+    cin >> start_time;
+    cout << "Select an end time(00:00:25) by duration.\n";
+    string end_time;
+    cin >> end_time;
+    string command = "ffmpeg -ss " + start_time + " -i " + file_name + " -t " + end_time + " -c copy " + file_name.substr(0, file_name.find_last_of(".")) + "_cut." + file_name.substr(file_name.find_last_of(".")+ 1);
+    exec_print(command.c_str());
+}
 int main(){
     cout << "What do you want to do?\n"
          << "Select by typing\n1\n"
          << "(1) Convert into another format.\n"
          << "(2) Extract audio.\n"
          << "(3) Extract video.\n"
-         << "(4) Cut video.\n";
+         << "(4) Cut video by length.\n"
+         << "(5) Cut video by duration.\n";
     int choice;
     cin >> choice;
     switch(choice){
@@ -124,7 +143,10 @@ int main(){
             extract_video();
             break;
         case 4:
-            cut_video();
+            cut_video_l();
+            break;
+        case 5:
+            cut_video_d();
             break;
         default:
             cout << "Invalid choice.\n";
